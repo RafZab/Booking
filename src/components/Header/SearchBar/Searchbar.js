a0628d313ad0d66ab8a31a99ff7243a2ecddec97
Rafał Zaborowski <raffik2000@gmail.com>
2021-06-30 00:22:13 +0200

    getting value with search bar in header

--- src/components/Header/SearchBar/Searchbar.js
+++ src/components/Header/SearchBar/Searchbar.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React ,{ useState } from 'react';
 
 function SearchBar() {
+    const [term, setTerm] = useState('')
+
+    const search = () => {
+        console.log('szukaj!', term)
+    }
+
     return (
         <div className="d-flex">
             <input
+                value={term}
+                onKeyDown={e => e.key === 'Enter' && search()}
+                onChange={e => setTerm(e.target.value)}
                 className="form-control"
                 type="text"
                 placeholder="Szukaj..." />
@@ -11,7 +20,8 @@ function SearchBar() {
                 style={{
                     marginLeft: '2px'
                 }}
-                className="btn btn-secondary">Szukaj</button>
+                className="btn btn-secondary"
+                onClick={search}>Szukaj</button>
         </div>
     );
 }
