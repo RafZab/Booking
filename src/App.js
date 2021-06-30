625c9330bf8d5c98fcf8a19f4770b5028dba5897
Rafał Zaborowski <raffik2000@gmail.com>
2021-06-30 10:48:16 +0200

    creating a hotel array in App

--- src/App.js
+++ src/App.js
@@ -2,15 +2,42 @@ import './App.css';   // it is possible by webpack.
 import Header from './components/Header/Header';
 import Menu from './components/Menu/Menu';
 import Hotels from './components/Hotels/Hotels';
+import { render } from '@testing-library/react';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Menu />
-      <Hotels />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      hotels: [
+        {
+          id: 1,
+          name: 'Pod akacjami',
+          city: 'Warszawa',
+          rating: '8.3',
+          description: 'opis jakis jeszcze nie wiem jaki ale wkrótce się dowiem',
+          image: ''
+        },
+        {
+          id: 2,
+          name: 'Dębowy',
+          city: 'Lublin',
+          rating: '8.8',
+          description: 'opis jakis jeszcze nie wiem jaki ale wkrótce się dowiem',
+          image: ''
+        }
+      ]
+    }
+  }
+  render(){
+    return (
+        <div className="App">
+          <Header />
+          <Menu />
+          <Hotels hotels={this.state.hotels} />
+        </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Hotels/Hotel/Hotel.js
+++ src/components/Hotels/Hotel/Hotel.js
@@ -2,7 +2,7 @@ import React from 'react';
 import style from './Hotel.module.css';
 import hotelImg from '../../../assets/images/hotel.jpg'
 
-function Hotel() {
+function Hotel(props) {
     return (
         <div className={`${style.hotel} card`}>
             <div className="card-body">
@@ -16,18 +16,18 @@ function Hotel() {
                     <div className="col-8">
                         <div className="row">
                             <div className="col">
-                                <p className={style.title}>Pensjonat</p>
-                                <snap className="badge-light">miasto</snap>
+                                <p className={style.title}>{props.name}</p>
+                                <snap className="badge-light">{props.city}</snap>
                             </div>
                             <div className="col text-right">
-                                <h5>Ocena: 8.3</h5>
+                                <h5>Ocena: {props.rating}</h5>
                                 <a href="#" className="btn btn-primary mt-2 px-5">Pokaż</a>
                             </div>
                         </div>
 
                     </div>
                     <div className="col-12">
-                        <p className={style.description}>opis dsfsdgfdhgdfhdgfhgdfhdgfhgfdhdfghgdfhdgfh</p>
+                        <p className={style.description}>{props.description}</p>
                     </div>
                 </div>
             </div>
